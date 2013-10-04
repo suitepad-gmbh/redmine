ba59d3e9574f7033056a91053d9aedc5f03e8b6c
Moritz Lawitschka <moritz@urbanvention.com>
2013-10-04 16:28:49 +0200

    Configure capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,101 @@
+require 'bundler/capistrano'
+require 'capistrano-unicorn'
+
+# Application settings
+set :application, 'redmine'
+set :user,        'suitepad'
+set :group,       'suitepad'
+
+# Repository settings
+set :scm,         :git
+set :repository,  'git@github.com:suitepad-gmbh/redmine.git'
+set :ssh_options, { :port => 2899 } # Use 2899 port instead of default 22
+set :branch,      'master'
+
+# Server settings
+server 'app-server-05.suitepad.de', :app, :web, :db, :primary => true
+set(:deploy_to) { "/home/suitepad/web/#{application}" }
+set :use_sudo,  false
+
+
+################################################################################
+# Set additional symbolic links, move files, backups stuff, set rackspace
+# config if needed
+################################################################################
+namespace :configure do
+  task :delete_database_config, :roles => :app do
+    run "rm #{current_release}/config/database.yml"
+  end
+
+  task :set_config_link, :roles => :app do
+    run "ln -s #{shared_path}/config/* #{current_release}/config/"
+  end
+
+  task :set_uploads_link, :roles => :app do
+    run "rm -rf #{current_release}/public/uploads"
+    run "ln -s #{shared_path}/uploads #{current_release}/public/uploads"
+  end
+
+  task :set_socket_symlink, :roles => :app do
+    run "rm -rf #{current_release}/tmp/sockets"
+    run "ln -s #{shared_path}/sockets #{current_release}/tmp/sockets"
+  end
+end
+
+
+################################################################################
+# Database tasks
+################################################################################
+namespace :db do
+
+  desc "Create Production Database"
+  task :create do
+    puts "\n\n=== Creating the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
+    system "cap deploy:set_permissions"
+  end
+
+  desc "Migrate Production Database"
+  task :migrate do
+    puts "\n\n=== Migrating the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:migrate RAILS_ENV=#{rails_env}"
+    system "cap deploy:set_permissions"
+  end
+
+  desc "Resets the Production Database"
+  task :migrate_reset do
+    puts "\n\n=== Resetting the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:migrate:reset RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Destroys Production Database"
+  task :drop do
+    puts "\n\n=== Destroying the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:drop RAILS_ENV=#{rails_env}"
+    system "cap deploy:set_permissions"
+  end
+
+  desc "Populates the Production Database"
+  task :seed do
+    puts "\n\n=== Populating the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+end
+
+
+# Run migrations after updating code
+before 'deploy:restart', 'deploy:migrate'
+
+# Tasks to do before asset compiling
+before 'deploy:assets:precompile' do
+  configure.delete_database_config
+  configure.set_config_link
+  configure.set_uploads_link
+  configure.set_socket_symlink
+end
+
+# After restart hooks
+after 'deploy:restart' do
+  unicorn.restart
+end
---
+++ config/unicorn/production.rb
@@ -0,0 +1,46 @@
+# Define your root directory
+root = "/home/suitepad/web/redmine/current"
+
+# Define worker directory for Unicorn
+working_directory root
+
+# Location of PID file
+pid "#{root}/tmp/pids/unicorn.pid"
+
+# Define Log paths
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+# Listen on a UNIX data socket
+listen "#{root}/tmp/sockets/app.socket"
+
+# 16 worker processes for production environment
+worker_processes 4
+
+# Load rails before forking workers for better worker spawn time
+preload_app true
+
+# Restart workes hangin' out for more than 240 secs
+timeout 120
+
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+# Force the bundler gemfile environment variable to
+# reference the Сapistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
